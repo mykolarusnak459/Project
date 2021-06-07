47bb96672c0de7b4faa47806fed97f7515dd08b5
mykolarusnak459 <mykolarusnak459@icloud.com>
2021-06-07 09:04:27 +0300

    test task

---
+++ JS/script.js
@@ -0,0 +1,90 @@
+
+
+
+const buttonIdElement = document.querySelector(`.button_id`);
+const buttonElements = buttonIdElement.querySelectorAll(`.button_user`);
+
+// Перебираем все элементы списка и присваиваем нужное значение
+for (const task of buttonElements) {
+    task.draggable = true;
+}
+
+buttonIdElement.addEventListener(`dragstart`, (evt) => {
+    evt.target.classList.add(`selected`);
+})
+
+buttonIdElement.addEventListener(`dragend`, (evt) => {
+    evt.target.classList.remove(`selected`);
+});
+
+buttonIdElement.addEventListener(`dragover`, (evt) => {
+    // Разрешаем сбрасывать элементы в эту область
+    evt.preventDefault();
+
+    // Находим перемещаемый элемент
+    const activeElement = buttonIdElement.querySelector(`.selected`);
+    // Находим элемент, над которым в данный момент находится курсор
+    const currentElement = evt.target;
+    // Проверяем, что событие сработало:
+    // 1. не на том элементе, который мы перемещаем,
+    // 2. именно на элементе списка
+    const isMoveable = activeElement !== currentElement &&
+        currentElement.classList.contains(`button_user`);
+
+    // Если нет, прерываем выполнение функции
+    if (!isMoveable) {
+        return;
+    }
+
+    // Находим элемент, перед которым будем вставлять
+    const nextElement = (currentElement === activeElement.nextElementSibling) ?
+        currentElement.nextElementSibling :
+        currentElement;
+
+    // Вставляем activeElement перед nextElement
+    buttonIdElement.insertBefore(activeElement, nextElement);
+});
+
+const getNextElement = (cursorPosition, currentElement) => {
+    // Получаем объект с размерами и координатами
+    const currentElementCoord = currentElement.getBoundingClientRect();
+    // Находим вертикальную координату центра текущего элемента
+    const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+
+    // Если курсор выше центра элемента, возвращаем текущий элемент
+    // В ином случае — следующий DOM-элемент
+    const nextElement = (cursorPosition < currentElementCenter) ?
+        currentElement :
+        currentElement.nextElementSibling;
+
+    return nextElement;
+};
+
+tasksListElement.addEventListener(`dragover`, (evt) => {
+    evt.preventDefault();
+
+    const activeElement = tasksListElement.querySelector(`.selected`);
+    const currentElement = evt.target;
+    const isMoveable = activeElement !== currentElement &&
+        currentElement.classList.contains(`button_user`);
+
+    if (!isMoveable) {
+        return;
+    }
+
+    // evt.clientY — вертикальная координата курсора в момент,
+    // когда сработало событие
+    const nextElement = getNextElement(evt.clientY, currentElement);
+
+    // Проверяем, нужно ли менять элементы местами
+    if (
+        nextElement &&
+        activeElement === nextElement.previousElementSibling ||
+        activeElement === nextElement
+    ) {
+        // Если нет, выходим из функции, чтобы избежать лишних изменений в DOM
+        return;
+    }
+
+    tasksListElement.insertBefore(activeElement, nextElement);
+});
